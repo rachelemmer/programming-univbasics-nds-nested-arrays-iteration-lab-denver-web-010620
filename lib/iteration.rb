a05fef91cbbbcc0fb2757136af75d6a71468cc6c
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-28 23:20:35 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,13 +7,6 @@ row_index += 1
 end 
 ingredients
 
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
 end
 
 def find_greater_pair(src)
