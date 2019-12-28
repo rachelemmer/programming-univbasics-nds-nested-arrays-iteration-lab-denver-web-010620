622abce3900a1ab754907fb5578dc186be198545
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-28 23:35:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,7 +13,7 @@ def find_greater_pair(src)
   new_array = []
   row_index = 0 
   while row_index < src.count do 
-    if src[row_index][0]>src[row_index][1]
+    if src[row_index][0] > src[row_index][1]
       new_array << src[row_index][0]
     else 
       new_array << src[row_index][1]
